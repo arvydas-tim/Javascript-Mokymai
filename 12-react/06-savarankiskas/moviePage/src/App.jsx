ae0b8f2ad61106cc504761a7e15cf23df674813c
arvydas-tim <arvydas.tm@gmail.com>
2024-10-31 10:29:54 +0200

    herpderp watchlistarray initial value set

--- 12-react/06-savarankiskas/moviePage/src/App.jsx
+++ 12-react/06-savarankiskas/moviePage/src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+if (localStorage.getItem("watchlistArray")==null) {
+  localStorage.setItem("watchlistArray", "");
+}
 function App() {
   const [movies, setMovies] = useState(null);
   const [error, setError] = useState(null);
@@ -116,7 +119,7 @@ function App() {
                   width="28"
                   height="28"
                   fill="currentColor"
-                  class="bi bi-x"
+                  className="bi bi-x"
                   viewBox="0 0 16 16"
                 >
                   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
